a5f954125404e11f28797f6ddd071496abe9fdc2
BloxyBuy <kbercicreator@freemail.hu>
2025-08-16 21:13:49 +0200

    Update tamper.js

--- tamper.js
+++ tamper.js
@@ -1,7 +1,10 @@
-   bot.on('kicked', (reason) =>
-      console.log(
-         '\x1b[33m',
-         `[AfkBot] Bot was kicked from the server. Reason: \n${reason}`,
-         '\x1b[0m'
-      )
+bot.on('kicked', (reason) => {
+   console.log(
+      '\x1b[33m',
+      `[AfkBot] Bot was kicked from the server. Reason: \n${reason}`,
+      '\x1b[0m'
    );
+
+   // Stop the project
+   process.exit(0);
+});
